50f9b8fe6b69e06cdff426087fa54cd9aa089104
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2019-09-05 11:32:35 +0700

    Initial Commit

---
+++ Other Script/answer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class answer : MonoBehaviour {
+    public int jawaban;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void button()
+    {
+        if (this.randomButton())
+        {
+            jawaban = 1;
+        }
+        else
+        {
+            jawaban = 0;
+        }
+        Debug.Log(jawaban);
+    }
+    
+    public bool randomButton()
+    {
+        float RandomJawaban = Random.Range(0.00f, 1.00f);
+        return (RandomJawaban >= 0.0f && RandomJawaban <= 0.5f);
+    }
+
+    public void question()
+    {   
+        string[] question = new string[3];
+        question[0] = "Apakah Jakarta adalah ibukota?";
+        question[1] = "Apakah bahasa C# lebih sukar dari C++?";
+        question[2] = "Apakah C# itu bahasa pemograman?";
+        
+
+    }
+}
---
+++ Other Script/skillscr.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skillscr : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float dashSpeed = 12.0f;
+    public float dashcd = 3.0f;
+
+    public float skillcd = 5.0f;
+    public bool cooldown = false;
+    public bool dashcooldown = false;
+
+
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            //Debug.Log("Nyentuh koq kak");
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+            speedDash();
+            //print("Horizontal: " + Input.GetAxis("Horizontal"));
+            //print("Vertical: " + Input.GetAxis("Vertical"));
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        DoubleJump();
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    void DoubleJump()
+    {
+        if ((!characterController.isGrounded) && (cooldown == false) && (skillcd > 0))
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("Skill Activated!!");
+                moveDirection.y = jumpSpeed;
+                cooldown = true;
+
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+        }
+        if (cooldown == true)
+        {
+            skillcd -= Time.deltaTime;
+            if (skillcd < 0)
+            {
+                skillcd = 5f;
+                cooldown = false;
+            }
+        }
+    }
+
+    void speedDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashcooldown == false)
+        {
+            Debug.Log("DASSHHHH!!!!");
+            moveDirection *= dashSpeed;
+            dashcooldown = true;
+        }
+        if(dashcooldown == true )
+        {
+            dashcd -= Time.deltaTime;
+            if(dashcd < 0)
+            {
+                dashcd = 3f;
+                dashcooldown = false;
+            }
+        }
+    }
+}
